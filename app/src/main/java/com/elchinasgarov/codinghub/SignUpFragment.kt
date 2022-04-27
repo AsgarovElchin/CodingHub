48bab6655bf617c1af62d1f7ddeeb0a5dc177ecb
ElchinAsgarov <79592128+AsgarovElchin@users.noreply.github.com>
2022-04-27 21:28:07 +0400

    login navigate half done - updates(icon color)

--- app/src/main/java/com/elchinasgarov/codinghub/SignUpFragment.kt
+++ app/src/main/java/com/elchinasgarov/codinghub/SignUpFragment.kt
@@ -5,8 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.elchinasgarov.codinghub.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    private lateinit var binding : FragmentSignUpBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginTv.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
+    }
 }
