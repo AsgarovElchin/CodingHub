4500ab243f208c6d40d407a260a1109fd4907140
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-05-15 10:05:51 +0400

    bottom navigation updated

--- app/src/main/java/com/elchinasgarov/codinghub/BottomNavigationFragment.kt
+++ app/src/main/java/com/elchinasgarov/codinghub/BottomNavigationFragment.kt
@@ -1,12 +1,33 @@
 package com.elchinasgarov.codinghub
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.elchinasgarov.codinghub.databinding.FragmentBottomNavigationBinding
 
 
 class BottomNavigationFragment : Fragment(R.layout.fragment_bottom_navigation) {
+    lateinit var binding: FragmentBottomNavigationBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBottomNavigationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navHost = childFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        binding.bnv.setupWithNavController(navHost.navController)
+        binding.bnv.setOnItemReselectedListener(null)
+    }
+
+
 
 }
